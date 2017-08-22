1df7894b78fad30c31b82563de4aeafce27a0527
Alexander Iundin <dorian.adret@gmail.com>
2017-08-22 17:25:58 +0300

    Add basic validates for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,27 @@
 class User < ApplicationRecord
+  VALID_NAME_REGEX = /\A[\w]+ [\w]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+[a-z\d]\.[a-z]+\z/i
+
+  validates :name, presence: true,
+            format: { with: VALID_NAME_REGEX }
+
+  validates :email, presence: true,
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: { case_sensitive: false }
+
+  before_save :admin_rule, if: :first_record?
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def first_record?
+    User.all.size == 0
+  end
+
+  private
+    def admin_rule
+      self.is_admin = true
+    end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryGirl.define do
   factory :user do
-    
+    sequence(:name)     { |n| "Name #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com"}
+    password 'password'
+    password_confirmation 'password'
   end
 end
